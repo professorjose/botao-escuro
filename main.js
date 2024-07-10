3c0104d19b7b4680a3a590d45f655d9afd16d30f
José Alves de Oliveira <160446328+professorjose@users.noreply.github.com>
2024-07-09 22:09:47 -0300

    .

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 const botao = document.querySelector(".botao"); 
-botao.onclick = function(){botao.classList.add("fundo-preto");}
+botao.onclick = function(){botao.classList.add("fundo-preto");};
+botao.onclick = function(){botao.classList.remove("fundo-preto");};
